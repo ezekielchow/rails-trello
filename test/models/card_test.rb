c2c280ff01187bef80885fdbc1ca2bb2cd8b0dca
Ezekiel Chow <ezekielchow94@gmail.com>
2018-12-06 15:09:11 +0800

    tested rails testing

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save card without name" do
+    card = Card.new
+    assert_not card.save, "saved card without name"
+  end
 end
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save list without name" do
+    list = List.new
+    assert_not list.save, "Saved the list without a name"
+  end
+
+  test "should report error" do
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+  end
 end
